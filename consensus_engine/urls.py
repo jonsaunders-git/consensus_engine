400684ef6d317c105ffed52fe9ff9cebace852bd
Jon Saunders <jonathansaunders@Jonathans-MacBook-Pro.local>
2020-04-09 13:55:02 +0100

    Added simple views

--- consensus_engine/urls.py
+++ consensus_engine/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('proposals/new/', views.new_proposal, name='new_proposal'),
     path('proposals/', views.list_proposals, name='list_proposals'),
+    path('proposals/<int:proposal_id>/', views.view_proposal, name='view_proposal')
 ]
--- consensus_engine/views.py
+++ consensus_engine/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Proposal
+
 # Create your views here.
 
 def index(request):
     return render(request, 'consensus_engine/index.html')
 
+def view_proposal(request, proposal_id):
+    return HttpResponse("You're looking at the view proposal page.")
 
 def new_proposal(request):
     return HttpResponse("You're looking at the enter proposal page.")
 
 def list_proposals(request):
-    return HttpResponse("You're looking at the list proposals page.")
+    proposals_list = Proposal.objects.order_by('-date_proposed')[:5]
+    context = {'proposals_list': proposals_list}
+    return render(request, 'consensus_engine/list_proposals.html', context)
