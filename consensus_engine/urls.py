c9397185bc11d4879c60474dc61bba57a6eb7b45
Jon Saunders <jonathansaunders@Jonathans-MacBook-Pro.local>
2020-04-09 14:52:56 +0100

    Set up basic authentication

--- consensus_engine/urls.py
+++ consensus_engine/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.urls import include
 
 from . import views
 
@@ -6,5 +7,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('proposals/new/', views.new_proposal, name='new_proposal'),
     path('proposals/', views.list_proposals, name='list_proposals'),
-    path('proposals/<int:proposal_id>/', views.view_proposal, name='view_proposal')
+    path('proposals/<int:proposal_id>/', views.view_proposal, name='view_proposal'),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
--- consensus_engine/views.py
+++ consensus_engine/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.contrib.auth.decorators import login_required
+
 from .models import Proposal
 
 # Create your views here.
@@ -8,12 +10,15 @@ from .models import Proposal
 def index(request):
     return render(request, 'consensus_engine/index.html')
 
+@login_required
 def view_proposal(request, proposal_id):
     return HttpResponse("You're looking at the view proposal page.")
 
+@login_required
 def new_proposal(request):
     return HttpResponse("You're looking at the enter proposal page.")
 
+@login_required
 def list_proposals(request):
     proposals_list = Proposal.objects.order_by('-date_proposed')[:5]
     context = {'proposals_list': proposals_list}
