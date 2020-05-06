b3a7b808eb5a2a09c95bc7afb3a7c9ffdef3ce45
Jon Saunders <jonathansaunders@Jonathans-MacBook-Pro.local>
2020-05-06 16:14:38 +0100

    Fixed bug about deactiveated choices.

--- consensus_engine/templatetags/proposaltags.py
+++ consensus_engine/templatetags/proposaltags.py
@@ -13,14 +13,14 @@ def visible_groups(user):
 # this needs refactoring as it calls multiple times per view...
 @register.inclusion_tag('consensus_engine/total_votes.html')
 def total_votes(proposal_id):
-    total_votes = ChoiceTicket.objects.filter(proposal_choice__proposal__id=proposal_id, current=1).count()
+    total_votes = ChoiceTicket.objects.filter(proposal_choice__deactivated_date__isnull=True, proposal_choice__proposal__id=proposal_id, current=1).count()
     return {'total_votes': total_votes}
 
 # this needs refactoring as it calls multiple times per view...
 @register.inclusion_tag('consensus_engine/my_vote.html')
 def my_vote(proposal_id, user_id):
     try:
-        my_vote = ProposalChoice.objects.get(proposal__id=proposal_id, choiceticket__user_id=user_id, choiceticket__current=True).text
+        my_vote = ProposalChoice.objects.get(deactivated_date__isnull=True, proposal__id=proposal_id, choiceticket__user_id=user_id, choiceticket__current=True).text
     except:
         my_vote = "None"
     return {'my_vote': my_vote }
