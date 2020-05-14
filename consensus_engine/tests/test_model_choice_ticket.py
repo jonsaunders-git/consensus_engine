00b27b19ce3958201435903943e5277f59dfbe94
Jon Saunders <jonathansaunders@Jonathans-MacBook-Pro.local>
2020-05-14 15:06:55 +0100

    Added some more tests on my_votes

--- consensus_engine/tests/test_model_choice_ticket.py
+++ consensus_engine/tests/test_model_choice_ticket.py
@@ -24,7 +24,6 @@ class ChoiceTicketTest(TwoUserMixin, ProposalMixin, TestCase):
         self.assertTrue(v.proposal_choice == pc1)
         self.assertTrue(v.current == True)
 
-
     def test_my_votes_with_with_voting(self):
         w = self.create_proposal_with_two_proposal_choices()
         # check that total votes = 0 if there are no votes
@@ -45,7 +44,8 @@ class ChoiceTicketTest(TwoUserMixin, ProposalMixin, TestCase):
         # test my_votes
         self.assertTrue(ChoiceTicket.objects.my_votes(self.user).count() == 1)
 
-    def test_my_votes_with_with_voting_with_a_deactivated_proposal_choice(self):
+
+    def test_my_votes_with_voting_with_a_deactivated_proposal_choice(self):
         w = self.create_proposal_with_two_proposal_choices()
         # check that total votes = 0 if there are no votes
         self.assertTrue(isinstance(w, Proposal))
@@ -75,3 +75,42 @@ class ChoiceTicketTest(TwoUserMixin, ProposalMixin, TestCase):
         pc2.save()
         x2 = ChoiceTicket.objects.my_votes(self.user)
         self.assertTrue(x2.count() == 0)
+
+
+    def test_my_votes_with_multiple_votes(self):
+        w = self.create_proposal_with_two_proposal_choices()
+        self.assertTrue(isinstance(w, Proposal))
+        pc1 = w.proposalchoice_set.first()
+        self.assertTrue(ChoiceTicket.objects.my_votes(self.user).count() == 0)
+        v = ChoiceTicket.objects.create(user=self.user, date_chosen=timezone.now(), proposal_choice=pc1, current=True)
+        self.assertTrue(isinstance(v, ChoiceTicket))
+        self.assertTrue(ChoiceTicket.objects.my_votes(self.user).count() == 1)
+        v3 = ChoiceTicket.objects.create(user=self.user2, date_chosen=timezone.now(), proposal_choice=pc1, current=True)
+        self.assertTrue(isinstance(v, ChoiceTicket))
+        self.assertTrue(ChoiceTicket.objects.my_votes(self.user2).count() == 1)
+        # test my_votes
+        self.assertTrue(ChoiceTicket.objects.my_votes(self.user).count() == 1)
+        # create another proposal to vote on
+        w2 = self.create_proposal_with_two_proposal_choices()
+        self.assertTrue(isinstance(w, Proposal))
+        pc2 = w2.proposalchoice_set.first()
+        pc3 = w2.proposalchoice_set.last()
+        self.assertTrue(ChoiceTicket.objects.my_votes(self.user).count() == 1)
+        v = ChoiceTicket.objects.create(user=self.user, date_chosen=timezone.now(), proposal_choice=pc2, current=True)
+        self.assertTrue(isinstance(v, ChoiceTicket))
+        self.assertTrue(ChoiceTicket.objects.my_votes(self.user).count() == 2)
+        # create another vote for another user
+        v3 = ChoiceTicket.objects.create(user=self.user2, date_chosen=timezone.now(), proposal_choice=pc3, current=True)
+        self.assertTrue(isinstance(v, ChoiceTicket))
+        # test my_votes
+        self.assertTrue(ChoiceTicket.objects.my_votes(self.user).count() == 2)
+        # deactivate the proposal_choice
+        pc2.deactivated_date = timezone.now()
+        pc2.save()
+        x2 = ChoiceTicket.objects.my_votes(self.user)
+        self.assertTrue(x2.count() == 1)
+        self.assertTrue(ChoiceTicket.objects.my_votes(self.user2).count() == 2)
+        # deactivate the proposal_choice
+        pc3.deactivated_date = timezone.now()
+        pc3.save()
+        self.assertTrue(ChoiceTicket.objects.my_votes(self.user2).count() == 1)
